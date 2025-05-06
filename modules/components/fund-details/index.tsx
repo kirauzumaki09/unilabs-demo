5a3b10157e19d37e1300b8c61a3e7911e7a42f75
rohansantra <rohan.santra@webskitters.in>
2025-05-06 16:57:45 +0530

    changes

--- modules/components/fund-details/index.tsx
+++ modules/components/fund-details/index.tsx
@@ -1,14 +1,40 @@
-import React from 'react'
-import BannerSection from './banner'
-import DetailsContent from './details-content'
+import React from "react";
+import BannerSection from "./banner";
+import DetailsContent from "./details-content";
+import DetailsCard from "./details-card";
+import { StaticImageData } from "next/image";
 
-const FundDetailsPage = () => {
-  return (
-    <>
-        <BannerSection />
-        <DetailsContent />
-    </>
-  )
+interface FundDetailsPageProps {
+  title: string;
+  date: string;
+  children: React.ReactNode;
+  image:StaticImageData
 }
 
-export default FundDetailsPage
+const FundDetailsPage = ({ children, date, title ,image}: FundDetailsPageProps) => {
+  return (
+    <>
+      <BannerSection image={image} />
+      <div className="details-cont pt-[86px] pb-[86px]">
+        <div className="max-w-[1529px] mx-auto w-full pl-5 pr-5">
+          <div className="flex flex-wrap ml-[-12px] mr-[-12px]">
+            <div className="w-[60%] pl-[12px] pr-[12px]">
+              <div className="w-full max-w-[869px]">
+                <span className="text-[18px] font-medium text-[#2DC4EC] block mb-[24px]">
+                  {date}
+                </span>
+                <h1 className="text-[50px] font-anybody font-semibold leading-[1.2] mb-[25px]">
+                  {title}
+                </h1>
+                <DetailsCard>{children}</DetailsCard>
+              </div>
+            </div>
+            <div className="w-[40%] pl-[12px] pr-[12px]"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FundDetailsPage;
