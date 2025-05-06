55840ebce4ae03ef388a428e7502832cb961dba3
rohansantra <rohan.santra@webskitters.in>
2025-05-06 17:23:38 +0530

    changes

--- modules/components/fund-details/index.tsx
+++ modules/components/fund-details/index.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import React from "react";
 import BannerSection from "./banner";
 import DetailsCard from "./details-card";
+import { fundList } from "../fund-main/fundlist.mock";
 
 interface FundDetailsPageProps {
   title: string;
@@ -35,37 +36,42 @@ const FundDetailsPage = ({
                 <DetailsCard>{children}</DetailsCard>
               </div>
             </div>
+
             <div className="w-[40%] pl-[12px] pr-[12px]">
               <h3 className="text-[24px] font-bold leading-[1.2] mb-[36px]">
                 Recent Update
               </h3>
-
-              <div className="flex flex-wrap">
-                <div className="w-[162px] h-[162px] overflow-hidden rounded-[14px]">
-                  <Link href="#" className="w-full h-full inline-block">
-                    <Image
-                      className="w-full h-full object-cover"
-                      src={assets.FundUpdate2}
-                      width={162}
-                      height={162}
-                      alt="recent-update-image"
-                    />
-                  </Link>
-                </div>
-                <div className="max-w-[calc(100%-162px)] pl-[32px] w-full">
-                  <p className="text-[18px] font-medium text-[#2DC4EC] leading-[1.2] mb-[16px]">
-                    1 May, 2025
-                  </p>
-                  <h3 className="text-[24px] font-semibold mb-[16px] font-anybody capitalize">
-                    <Link className="inline-block" href="#">
-                      Fund Update 2
-                    </Link>
-                  </h3>
-                  <p className="text-[18px] leading-[1.8] capitalize">
-                    Our team is excited to share today’s progress on building
-                    the foundation...
-                  </p>
-                </div>
+              <div className="flex flex-col gap-[40px]">
+                {fundList
+                  .filter((item) => item.title !== title)
+                  .map((item, index) => (
+                    <div className="flex flex-wrap" key={index}>
+                      <div className="w-[162px] h-[162px] overflow-hidden rounded-[14px]">
+                        <Link href="#" className="w-full h-full inline-block">
+                          <Image
+                            className="w-full h-full object-cover"
+                            src={item.image}
+                            width={162}
+                            height={162}
+                            alt="recent-update-image"
+                          />
+                        </Link>
+                      </div>
+                      <div className="max-w-[calc(100%-162px)] pl-[32px] w-full">
+                        <p className="text-[18px] font-medium text-[#2DC4EC] leading-[1.2] mb-[16px]">
+                          {item.date}
+                        </p>
+                        <h3 className="text-[24px] font-semibold mb-[16px] font-anybody capitalize">
+                          <Link className="inline-block" href="#">
+                            {item.title}
+                          </Link>
+                        </h3>
+                        <p className="text-[18px] leading-[1.8] capitalize lineclamp2">
+                          {item.description}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
               </div>
             </div>
           </div>
