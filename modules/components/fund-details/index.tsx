fb1fad13378c29998377629ca93407202528e697
rohansantra <rohan.santra@webskitters.in>
2025-05-06 17:05:44 +0530

    changes

--- modules/components/fund-details/index.tsx
+++ modules/components/fund-details/index.tsx
@@ -18,12 +18,12 @@ const FundDetailsPage = ({ children, date, title ,image}: FundDetailsPageProps) 
       <div className="details-cont pt-[86px] pb-[86px]">
         <div className="max-w-[1529px] mx-auto w-full pl-5 pr-5">
           <div className="flex flex-wrap ml-[-12px] mr-[-12px]">
-            <div className="w-[60%] pl-[12px] pr-[12px]">
+            <div className="w-[60%] max-lg:w-full pl-[12px] pr-[12px]">
               <div className="w-full max-w-[869px]">
                 <span className="text-[18px] font-medium text-[#2DC4EC] block mb-[24px]">
                   {date}
                 </span>
-                <h1 className="text-[50px] font-anybody font-semibold leading-[1.2] mb-[25px]">
+                <h1 className="text-[50px] max-lg:text-[40px] max-md:text-[32px] font-anybody font-semibold leading-[1.2] mb-[25px]">
                   {title}
                 </h1>
                 <DetailsCard>{children}</DetailsCard>
--- modules/components/fund-main/fundlist.mock.ts
+++ modules/components/fund-main/fundlist.mock.ts
@@ -7,7 +7,7 @@ export const fundList = [
     description:
       "Today, we are excited to share the very first steps in building Unilabs — the next-generation AI-powered DeFi asset manager. Our presale platform is already live, but now begins the full rollout of the autonomous systems that will define Unilabs over the next 90 days. Here’s what we did today.",
     image: assets.fundUpdate1,
-    link: "/fund-updates-1",
+    link: "/fund-update-1",
   },
   {
     title: "Fund Update 2",
@@ -15,7 +15,7 @@ export const fundList = [
     description:
       "Our team is excited to share today’s progress on building the foundation of our AI-powered DeFi asset management system. Every move we make is geared toward creating a platform that adapts, self-optimizes, and consistently delivers value without needing manual control. Here’s where we stand today.",
     image: assets.fundUpdate2,
-    link: "/fund-updates-2",
+    link: "/fund-update-2",
   },
   {
     title: "Fund Update 3",
@@ -23,7 +23,7 @@ export const fundList = [
     description:
       "Today marked an exciting leap for our team at Unilabs. We officially transitioned from theoretical modeling to live simulation testing for our AI-powered DeFi management systems. This move is crucial as we work toward making Unilabs Funds ready for real-world performance. Here’s what we achieved today. ",
     image: assets.fundUpdate3,
-    link: "/fund-updates-3",
+    link: "/fund-update-3",
   },
   {
     title: "Fund Update 4",
@@ -31,6 +31,6 @@ export const fundList = [
     description:
       "Today, we are excited to share the very first steps in building Unilabs — the next-generation AI-powered DeFi asset manager. Our presale platform is already live, but now begins the full rollout of the autonomous systems that will define Unilabs over the next 90 days. Here’s what we did today.",
     image: assets.fundUpdate4,
-    link: "/fund-updates-4",
+    link: "/fund-update-4",
   },
 ];
